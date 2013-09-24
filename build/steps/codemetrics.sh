69cf2c175f0e99d1ba536a0f8b6e3898e2228e57
kristoffer Richardsson <kristoffer.richardsson@203webgroups.se>
2013-09-24 11:21:49 +0200

    Added depend

--- build/steps/codemetrics.sh
+++ build/steps/codemetrics.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Composer
+php composer.phar install
+
 # Measure project size using PHPLOC
 phploc --log-csv ${WORKSPACE}/build/logs/phploc.csv ${WORKSPACE}/src
 
 # Calculate software metrics using PHP_Depend
-# pdepend --jdepend-xml=build/logs/jdepend.xml --jdepend-chart=build/pdepend/dependencies.svg --overview-pyramid=build/pdepend/overview-pyramid.svg src
+vendor/pdepend/pdepend/src/bin/pdepend --jdepend-xml=${WORKSPACE}/build/logs/jdepend.xml --jdepend-chart=${WORKSPACE}/build/pdepend/dependencies.svg --overview-pyramid=${WORKSPACE}/build/pdepend/overview-pyramid.svg ${WORKSPACE}/src
 
 # Perform project mess detection using PHPMD creating a log file for the continuous integration server
 # phpmd src xml build/phpmd.xml --reportfile build/logs/pmd.xml

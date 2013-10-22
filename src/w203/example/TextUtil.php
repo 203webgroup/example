6cbb5f345f724ec2c40f457c40b8ede59a61b18c
Kristoffer Richardsson <kristoffer@richardsson.org>
2013-10-22 06:38:00 +0200

    Added simple test

--- src/w203/example/TextUtil.php
+++ src/w203/example/TextUtil.php
@@ -6,4 +6,8 @@ namespace w203\example;
 
 class TextUtil {
 
+    public function formatProductAndPrice($product, $price) {
+        return $product . ", " . $price . " kr";
+    }
+
 }
--- tests/w203/example/TextUtilTest.php
+++ tests/w203/example/TextUtilTest.php
@@ -12,4 +12,18 @@ class TextUtilTest extends PHPUnit_Framework_TestCase {
     public function setUp() {
         $this->target = new TextUtil();
     }
+
+    /**
+     * @test
+     */
+    public function shouldFormatProductAndPrice() {
+        // Fixture
+
+        // Test
+        $actual = $this->target->formatProductAndPrice("Korv", 10);
+
+        // Assert
+        $this->assertEquals("Korv, 10 kr", $actual);
+    }
+
 }

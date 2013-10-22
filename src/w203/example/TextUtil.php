6480184a29e699a7d2751069e64b71bd44bbcef5
Kristoffer Richardsson <kristoffer@richardsson.org>
2013-10-22 06:49:15 +0200

    refactoring

--- src/w203/example/TextUtil.php
+++ src/w203/example/TextUtil.php
@@ -6,12 +6,37 @@ namespace w203\example;
 
 class TextUtil {
 
+    const highPriceLimit = 1000;
+
+    /**
+     * @param $product
+     * @param $price
+     * @return string
+     */
     public function formatProductAndPrice($product, $price) {
-        if ($price < 1000) {
-            return $product . ", " . $price . " kr";
+        if ($price < self::highPriceLimit) {
+            return $this->formatLowPrice($product, $price);
         } else {
-            return $product . ", " . round($price / 1000) . " tusen pix";
+            return $this->formatHighPrice($product, $price);
         }
+    }
+
+    /**
+     * @param $product
+     * @param $price
+     * @return string
+     */
+    public function formatLowPrice($product, $price) {
+        return $product . ", " . $price . " kr";
+    }
+
+    /**
+     * @param $product
+     * @param $price
+     * @return string
+     */
+    public function formatHighPrice($product, $price) {
+        return $product . ", " . round($price / self::highPriceLimit) . " tusen pix";
     }
 
 }
